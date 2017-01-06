8390c6d0030da862cc63bb4facb9d71217d727a4
Yordan Nedelchev <yordan.nedelchev@hotmail.com>
2017-01-06 02:22:14 +0200

    Information about some of the more special days during the year.

---
+++ phpsite/infobg/01-04.php
@@ -0,0 +1,9 @@
+Според Георги Велев и неговата книга 
+<a href="http://bookshop.bg/bg/knigi/nehudojestveni-izdanija/folklor-i-etnologija/narodniqt-kalendar-i-negovite-praznici-23917">"Народният календар и неговите празници"</a>
+на този ден по Древния Български Календар се е чествало пролетното равноденствие.
+<br/><br/>
+Символиката на този празник е била пренесена в християнската традиция под формата на празника 
+<a href="https://bg.wikipedia.org/wiki/%D0%91%D0%BB%D0%B0%D0%B3%D0%BE%D0%B2%D0%B5%D1%89%D0%B5%D0%BD%D0%B8%D0%B5">Благовещение</a>
+ (наричан още <i>Благовец</i>), 
+празнуван в днешно време от православната църква на 
+<a href="http://bg-patriarshia.bg/calendar.php?month=2">25<sup>-ти</sup> Март</a>.
