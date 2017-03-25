f27518308c766aa6337fd2a869c7652004b65a8d
Yordan Nedelchev <yordan.nedelchev@hotmail.com>
2017-03-26 01:44:37 +0200

    Start translation on imennit.html and kalendar.html

--- phpsite/index.php
+++ phpsite/index.php
@@ -1005,7 +1005,7 @@ $subperiods = ( isset($periodsbg[2]) && $periodsbg[2]->getStructure() != null) ?
            <td class="calendartable" 
                    colspan="7" 
                    style="text-align: center;">
-                  Месец Десети
+                  <?php tr('Месец Десети', 'Tenth Month', 'Zehnten Monat', 'Десятый Месяц');?>
                </td>
          </tr>
            <tr class="calendartable">
@@ -1070,7 +1070,7 @@ $subperiods = ( isset($periodsbg[2]) && $periodsbg[2]->getStructure() != null) ?
            <td class="calendartable" 
                    colspan="7" 
                    style="text-align: center;">
-                 <?php tr('Месец Единайсти', 'Eleventh Month', 'Elfte Monat', 'Одиннадцатый Месяц');?>
+                 <?php tr('Месец Единайсти', 'Eleventh Month', 'Elften Monat', 'Одиннадцатый Месяц');?>
                </td>
          </tr>
            <tr class="calendartable">
@@ -1132,7 +1132,7 @@ $subperiods = ( isset($periodsbg[2]) && $periodsbg[2]->getStructure() != null) ?
        <div class="month">
            <table class="calendartable">
         <tr class="calendartable">
-          <td class="calendartable" colspan="7" style="text-align: center;"><?php tr('Месец Дванайсти', 'Twelfth Month', 'Zwölfte Monat', 'Двенадцатый Месяц');?> </td>
+          <td class="calendartable" colspan="7" style="text-align: center;"><?php tr('Месец Дванайсти', 'Twelfth Month', 'Zwölften Monat', 'Двенадцатый Месяц');?> </td>
         </tr>
         <tr class="calendartable">
                <td class="calendarweekrow dayofweek"><div class="calendarvertical dayofweek">1<sup><?php tr('-ви','-st','-te','-ый');?></sup></div></td>
