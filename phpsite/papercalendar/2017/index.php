d6223b1174d1b341925081acf569701072be4e83
Yordan Nedelchev <yordan.nedelchev@hotmail.com>
2017-11-20 02:03:45 +0200

    Fix the view  style sheets for the paper calendar for 2017

---
+++ phpsite/papercalendar/2017/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+   <link rel="stylesheet" type="text/css" href="../../navigation.css" />
+   <link rel="stylesheet" type="text/css" href="../../bgkalendar.css" />
+
+   <title>Календар за 7522/2017 година</title>
+</head>
+<body class="body">
+<nav>
+<?php include(dirname(dirname(__DIR_)).'/../../navigation.php');?>
+</nav>
+<br/>
+
+<br/><br/>
+Благодарение на Анелия Донева, на тази страница можете да намерите за сваляне PDF версия на хартиен календар за година 7522 от Българския календар съответстваща на 2017 година по съвременния Грегориански календар. <br/>
+
+<br/><br/>
+Като един PDF <a href="https://drive.google.com/file/d/0B3EfZIS-ybD6X2hoMElHMjZKQk0/view">Виж</a> &nbsp; &nbsp; <a href="KAЛEHDAP-2017-7522.pdf">Свали</a>
+<br/><br/>
+<a href="../../../" width="50%">Обратно</a>
+<hr/>
+<table border="0">
+  <tr><td><ul style="margin-top: 0px; margin-bottom: 0px; padding-top: 0px; padding-bottom: 0px;"><li style="margin:0px; padding: 0px;">Първо Тримесечие    </li></ul></td>
+      <td><a href="calendar-1Q.pdf">Свали PDF</a></td>
+      <td><a href="calendar-1Q.pdf"><img src="1.png"/></a></td>
+      <td><a href="calendar-1Q.pdf"><img src="2.png"/></a></td>
+      <td><a href="calendar-1Q.pdf"><img src="3.png"/></a></td>
+  </tr>
+  <tr><td><ul style="margin-top: 0px; margin-bottom: 0px; padding-top: 0px; padding-bottom: 0px;"><li style="margin:0px; padding: 0px;">Второ Тримесечие    </li></ul></td>
+      <td><a href="calendar-2Q.pdf">Свали PDF</a></td>
+      <td><a href="calendar-2Q.pdf"><img src="4.png"/></a></td>
+      <td><a href="calendar-2Q.pdf"><img src="5.png"/></a></td>
+      <td><a href="calendar-2Q.pdf"><img src="6.png"/></a></td>
+  </tr>
+  <tr><td><ul style="margin-top: 0px; margin-bottom: 0px; padding-top: 0px; padding-bottom: 0px;"><li style="margin:0px; padding: 0px;">Трето Тримесечие    </li></ul></td>
+      <td><a href="calendar-3Q.pdf">Свали PDF</a></td>
+      <td><a href="calendar-3Q.pdf"><img src="7.png"/></a></td>
+      <td><a href="calendar-3Q.pdf"><img src="8.png"/></a></td>
+      <td><a href="calendar-3Q.pdf"><img src="9.png"/></a></td>
+  </tr>
+  <tr><td><ul style="margin-top: 0px; margin-bottom: 0px; padding-top: 0px; padding-bottom: 0px;"><li style="margin:0px; padding: 0px;">Четвърто Тримесечие </li></ul></td>
+      <td><a href="calendar-4Q.pdf">Свали PDF</a></td>
+      <td><a href="calendar-4Q.pdf"><img src="10.png"/></a></td>
+      <td><a href="calendar-4Q.pdf"><img src="11.png"/></a></td>
+      <td><a href="calendar-4Q.pdf"><img src="12.png"/></a></td>
+  </tr>
+</table>
+<hr/>
+<a href="../../../" width="50%" style="width: 60px;">Обратно</a>
+
+<br/>
+<br/>
+<?php include(dirname(dirname(__DIR__)).'/../../footer.php');?>
+</body>
+</html>
