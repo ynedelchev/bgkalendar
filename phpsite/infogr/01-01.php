8e7065ff53d9ffd72f0433e15ef58690aefdba76
Yordan Nedelchev <yordan.nedelchev@hotmail.com>
2018-10-07 19:04:54 +0300

    Bit more translations

--- phpsite/infogr/01-01.php
+++ phpsite/infogr/01-01.php
@@ -1,3 +1,5 @@
+
+<?php if ($lang == 'bg') : ?>
 Според Българският народен календар - <a href="https://bg.wikipedia.org/wiki/%D0%A1%D1%83%D1%80%D0%B2%D0%B0%D0%BA%D0%B8">Суроваки/Сурваки</a> (или още <i>Васильовден</i>). <br/>
 Предполагаемият първообраз на този ден е денят който е бил първи ден от първият
  месец на годината по Древния Български Календар. Този ден е бил непосредствно 
@@ -13,3 +15,26 @@
 Юлианския календар.
 <br/><br/>
 Георги Велев намира връзка в същността на празника и източната идея за жизнената сила "ки". Според него името "Суроваки", буквално означава "сурова ки", защото именно в този момент започва новият кръговрат на жизнената енергия.
+
+<?php elseif ($lang == 'en') : ?>
+According to the Bulgarian traditional calendar this day is named <a href="https://en.wikipedia.org/wiki/Survakane">Surovaki or Survaki</a> (also <i>Vasilyovday</i>). <br/>
+The supposed prototype of this day is is the day that was the first day of the first month of the year under the Ancient Bulgarian Calendar. 
+This day was just after the day Eni (equivalent to Ignazhden/Ednazhden).
+According to  
+<a href="https://spisanie8.bg/%D1%80%D1%83%D0%B1%D1%80%D0%B8%D0%BA%D0%B8/%D0%B7%D0%B0%D0%B3%D0%B0%D0%B4%D0%BA%D0%B8/2503-%D0%B4%D0%B0%D0%BE-%D0%BD%D0%B0-%D0%B1%D1%8A%D0%BB%D0%B3%D0%B0%D1%80%D1%81%D0%BA%D0%B8%D1%8F-%D0%BA%D0%B0%D0%BB%D0%B5%D0%BD%D0%B4%D0%B0%D1%80.html">
+Georgi Velev</a>, the adoption of the 
+<a href="https://en.wikipedia.org/wiki/Julian_calendar">
+Julian calendar</a> (later replaced by the <a href="https://en.wikipedia.org/wiki/Gregorian_calendar">Gregorian calendar</a>) and the abandonment of the Ancient Bulgarian Calendar, 
+the Surovaki selebration has kept its numeric value 01-01 (first day of the First Month), but has moved with ten days due to the difference in the beginning of the year under the Ancient Bulgarian Calendar and the Julian calendar.
+<br/><br/>
+Georgi Velev finds a connection in the essence of the traditiona in that day  and the Eastern idea of the life force "ki". According to him, the name "Surovaki" literally means "raw ki" (<a href="https://translate.google.com/#el/en/%D1%81%D1%83%D1%80%D0%BE%D0%B2%D0%B0">"surova"</a> means row in Bulgarian), because it is at this point that the new circle of vital energy begins.
+<?php elseif ($lang == 'de') : ?>
+Laut dem bulgarischen Volkskalender - <a href="https://de.wikipedia.org/wiki/Surwa">Surrowaki/Surwaki</a> (oder Vasilyovden).
+Der angebliche Prototyp dieses Tages ist der Tag, der der erste Tag des ersten Monats des Jahres unter dem alten bulgarischen Kalender war.
+Dieser Tag war nach dem Tag Eni (gleichwertig Ignazhden/Ednazhden).
+Laut <a href="https://spisanie8.bg/%D1%80%D1%83%D0%B1%D1%80%D0%B8%D0%BA%D0%B8/%D0%B7%D0%B0%D0%B3%D0%B0%D0%B4%D0%BA%D0%B8/2503-%D0%B4%D0%B0%D0%BE-%D0%BD%D0%B0-%D0%B1%D1%8A%D0%BB%D0%B3%D0%B0%D1%80%D1%81%D0%BA%D0%B8%D1%8F-%D0%BA%D0%B0%D0%BB%D0%B5%D0%BD%D0%B4%D0%B0%D1%80.html">Georgi Welew</a>, die Annahme des <a href="https://de.wikipedia.org/wiki/Julianischer_Kalender">Julianischen Kalenders</a> (später durch den <a href="https://de.wikipedia.org/wiki/Gregorianischer_Kalender">Gregorianischen Kalender</a> ersetzt) und die Aufgabe des alten bulgarischen Kalenders, hat der Surva-Feiertag seinen Zahlenwert von 01-01 (erster Tag des ersten Monats) beibehalten, hat aber Er bewegte sich zehn Tage lang wegen des Unterschieds zu Beginn des Jahres auf dem antiken bulgarischen Kalender und dem julianischen Kalender. 
+<br/><br/>
+Georgi Velev findet Verbindung im Wesen des Festivals und die östliche Idee der Lebenskraft "ki". Ihm zufolge bedeutet der Name "<a href="https://translate.google.com/#el/de/%D1%81%D1%83%D1%80%D0%BE%D0%B2%D0%B0">surowa</a>ki" wörtlich "roh ki", weil in diesem Moment der neue Zyklus der Lebensenergie beginnt.
+<?php elseif ($lang == 'ru') : ?>
+<?php endif ?>
+
