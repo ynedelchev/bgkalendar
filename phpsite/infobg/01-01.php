bef1bd47b1f67db8a8f5dd46749e408a3d756044
Yordan Nedelchev <yordan.nedelchev@hotmail.com>
2019-01-06 01:22:34 +0200

    A bit more of a translation

--- phpsite/infobg/01-01.php
+++ phpsite/infobg/01-01.php
@@ -9,7 +9,7 @@
 <b>Първо Полугодие нечетни</b><br/>
 1<sup>-ви</sup> ден Първи Месец (1.I), 3<sup>-ти</sup> ден Трети Месец (3.III) и 5<sup>-ти</sup> ден Пети Mесец (5.V) в българският календар са винаги в първия ден от седмицата, 
 <br/>
-<b>Второ Полугодие нечетни</b><br/>
+<b>Второ Полугодие четни</b><br/>
 7<sup>-ми</sup> ден Седми Месец (7.VII), 9<sup>-ти</sup> ден Девети Месец (9.IX) и 11<sup>-ти</sup> ден Единайсти Месец (11.XI) са винаги във последния (7<sup>-ми</sup>) ден от седмицата.
 <br/>
 Той приема тези дни - първият (според него неделя) и последния (събота) ден от седмицата за официални "мъжки" празници на които са се вземали важни решения за държавата
@@ -19,7 +19,62 @@
 
 
 <?php elseif ($lang == 'en') : ?>
-  
+This day (first day of the First Month (1. I) According to <a href="http://www.bookpoint.bg/книги/Българският-календар-14084.htm">Yordan Valchev</a> was related to the ancient custom "Koleda" (Christmas) and the slaughter of a pig. According to him the name "Koleda" comes from bulgarian word "kolya" meaning "slaughter". This day was always on the first day of the Bulgarian week - according to Yordan Valchev - that is Sunday. It was a second consecutive day after the day "Eni" (New Year, Ednazhden, Ignazhden). Elderly people remember that "the pig is slaughtered on the next Sunday after Ignazhden (currently being celebrated on 20-th of Deccember in Gregorian Callendar).
+<br/>
+In later times the rituals of this festival were transferred to the Christian Holiday (Christmas) celebrated on 25 December.
+<br/><br/>
+He has noticed that on the Bulgarian calendar The odd dates where the number of the day coincides with the number of the month 
+<br/>
+<b>First half of the year odd</b><br/>
+1<sup>-st</sup> day of the First Month (1.I), 3<sup>-rd</sup> day of the Third Month (3.III) and 5<sup>-th</sup> day of the Fifth Month (5.V) in the Bulgarian Callendar are always on the first day of the Bulgarian week, 
+<br/>
+<b>Second half of the year even</b><br/>
+7<sup>-th</sup> day of the Seventh Month (7.VII), 9<sup>-th</sup> day of the Nineth Month (9.IX) and 11<sup>-th</sup> day of the Eleventh Month (11.XI) are always on the last (7<sup>-th</sup>) day of the Bulgarian week.
+<br/>
+He assumes these days - the first (according to him Sunday) and the last (Saturday) to be the official "men's" holidays when major decisions for the state have been taken.
+<br><br/>
+On the other hand, the even dates where the day and month numbers match 2.II, 4.IV, 6.VI, 8.VIII, 10.X, and 12.XII are always in weekday days (different than the first and last of week) and were conditionally called "women's" holidays related to the household or family.
+<br/><br/>
+
+
+
 <?php elseif ($lang == 'de') : ?>
+An diesem Tag (1. Tag des Ersten Monats (1. I) war nach <a href="http://www.bookpoint.bg/книги/Българският-календар-14084.htm">Jordan Walchew</a> mit dem alten Brauch "Koleda" (Weihnachten) und dem Schlachten eines Schweins verwandt, nach ihm der Name "Koleda" stammt von der bulgarischen "kolya" (Massaker). Dieser Tag war immer am ersten Tag der bulgarischen Woche - laut Jordan Walchew - Sonntag. Es war ein zweiter Tag in Folge nach dem Tag "Eni" (Neujahr, Ednazhden, Ignazhden). Die Älteren erinnern sich, dass das "Schwein Autos am Sonntag nach Ignažden" ist. 
+<br/>
+In späteren Zeiten wurden die Rituale dieses Festes auf den am 25. Dezember gefeierten christlichen Feiertag (Weihnachten) übertragen. 
+<br/><br/>
+Er hat festgestellt, dass im bulgarischen Kalender die ungeraden Daten, an denen die Nummer des Tages mit der Nummer des Monats übereinstimmt
+<br/>
+<b>Erste Halbjahr ungerade</b><br/>
+1<sup>-te</sup> Tag Erster Monat (1.I), 3<sup>-te</sup> Tag Dritter Monat (3.III) und 5<sup>-te</sup> Tag 5. Monat (5.V) im bulgarischen Kalender, immer am ersten Tag der Woche liegen
+<br/>
+<b>Zweite Halbjahr abends</b><br/>
+7<sup>-te</sup> Tag Sieben Monate (7.VII), 9<sup>-te</sup> Tag Neunter Monat (9.IX) und 11<sup>-te</sup> Tag Elf Monate (11.XI), immer am letzten (7<sup>-sten</sup>) Tag der Woche liegen.
+<br/>
+Er akzeptiert diese Tage-den ersten (nach ihm Sonntag) und den letzten (Samstag) Tag der Woche für offizielle "Männer" Feiertage, die wichtige Entscheidungen für den Staat getroffen haben.
+<br><br/>
+Auf der anderen Seite auch Daten, wo die Anzahl des Tages und die Anzahl des Monats übereinstimmen 2. II, 4. IV, 6.VI, 8. VIII, 10. X und 12. XII sind immer an provisorischen Wochentagen (außer dem ersten und letzten der Woche) und waren bedingte "weibliche" Urlaub mit dem Haushalt oder Familie.
+<br/><br/>
+
+
+
 <?php elseif ($lang == 'ru') : ?>
+Этот день (1<sup>-й</sup> день первого месяца (1.I)), по словам <a href="http://www.bookpoint.bg/книги/Българският-календар-14084.htm">Йордана Вальчева</a>, был связан с древним обычаем Коледа и убоя свиньи. Он считает именем «Коледа» от «коля» (убоя). Этот день всегда был в первый день болгарской недели, по словам Йордана Вальчева - воскресенье. Это был второй день подряд после дня "Ени" (канун Нового года, Игнажден). Пожилые люди помнят, что «свинья должна быть убита на воскресенья после Игназдена».
+<br/>
+В более поздние времена обряды этого праздника были перенесены на рождественский праздник Рождества (Коледа), отмечаемый 25 декабря.
+<br/><br/>
+Он заметил, что в болгарском календаре нечетные даты, где число дня совпадает с номером месяца
+<br/>
+<b>Първо Полугодие нечетни</b><br/>
+1<sup>-ви</sup> ден Първи Месец (1.I), 3<sup>-ти</sup> ден Трети Месец (3.III) и 5<sup>-ти</sup> ден Пети Mесец (5.V) в българският календар, всегда в первый день недели
+<br/>
+<b>Второ Полугодие четни</b><br/>
+7<sup>-й</sup> день седьмой месяц (VII), 9<sup>-й</sup> день девятый месяц (9. IX) и 11<sup>-й</sup> день Единадцтой месяц (11. XI), всегда находится на последнем (седьмом) день недели.
+<br/>
+Он принимает эти дни-первый (по его мнению воскресенье) и последний (суббота) день недели для официальных "мужских" праздников, когда принимали важные решения для государства.
+<br><br/>
+С другой стороны, даты, где номер дня и номер месяца совпадают 2. II, 4. IV, 6.VI, 8. VIII, 10. X и 12. XII всегда находятся в предварительных будних днях (кроме первой и последней недели) и были условными "женскими" праздниками, связанными с домашним хозяйством или Семьи.
+<br/><br/>
+
+
 <?php endif ?>
--- phpsite/infobg/01-04.php
+++ phpsite/infobg/01-04.php
@@ -1,3 +1,4 @@
+<?php if ($lang == 'bg') : ?>
 Според Георги Велев и неговата книга 
 <a href="http://bookshop.bg/bg/knigi/nehudojestveni-izdanija/folklor-i-etnologija/narodniqt-kalendar-i-negovite-praznici-23917">"Народният календар и неговите празници"</a>
 на този ден по Древния Български Календар се е чествало пролетното равноденствие.
@@ -7,3 +8,40 @@
  (наричан още <i>Благовец</i>), 
 празнуван в днешно време от православната църква на 
 <a href="http://bg-patriarshia.bg/calendar.php?month=2">25<sup>-ти</sup> Март</a>.
+
+
+<?php elseif ($lang == 'en') : ?>
+According to Georgy Velev and his book 
+<a href="http://bookshop.bg/bg/knigi/nehudojestveni-izdanija/folklor-i-etnologija/narodniqt-kalendar-i-negovite-praznici-23917">"The Folk Calendar and its Holidays"</a>
+Oon this day the Spring Equinox was celebrated on the Ancient Bulgarian Calendar.
+<br/><br/>
+The symbolism of this celebration has been brought into the Christian tradition in the form of the feast
+<a href="https://en.wikipedia.org/wiki/Annunciation">Annunciation</a>
+ (also called <i>Blagovets</i> in Bulgarian), 
+celebrated in modern times by the Orthodox Church on
+<a href="http://bg-patriarshia.bg/calendar.php?month=2">25<sup>-th</sup> of March</a>.
+
+
+<?php elseif ($lang == 'de') : ?>
+Nach Georgi Welew und seinem Buch
+<a href="http://bookshop.bg/bg/knigi/nehudojestveni-izdanija/folklor-i-etnologija/narodniqt-kalendar-i-negovite-praznici-23917">"Volkskalender und seine Feiertage"</a>
+An diesem Tag war der alte bulgarische Kalender eine Feier der Frühlingstagung.
+<br/><br/>
+Die Symbolik dieser Feier wurde in Form des Festes in die christliche Tradition eingebracht
+<a href="https://de.wikipedia.org/wiki/Verkündigung_des_Herrn">Verkündigung</a> (Auch <i>Blagovets</i> genannt), In der Neuzeit von der orthodoxen Kirche der
+<a href="http://bg-patriarshia.bg/calendar.php?month=2">25<sup>-te</sup> März</a>.
+
+<?php elseif ($lang == 'ru') : ?>
+По словам Георгия Велева и его книги
+<a href="http://bookshop.bg/bg/knigi/nehudojestveni-izdanija/folklor-i-etnologija/narodniqt-kalendar-i-negovite-praznici-23917">"Народный календарь и его праздники"</a>
+В этот день по древний болгарский календарь праздновалось весеннего равноденствия.
+<br/><br/>
+Символика этого праздника была принесена в христианскую традицию в виде праздника
+<a href="https://ru.wikipedia.org/wiki/Благовещение_Пресвятой_Богородицы">Благовещение</a>
+ (также называемый <i>Благовец</i>),
+Отмечаемый в наше время православной церковью
+<a href="http://bg-patriarshia.bg/calendar.php?month=2">25<sup>-ого</sup> Mарта</a>.
+
+
+
+<?php endif ?>
