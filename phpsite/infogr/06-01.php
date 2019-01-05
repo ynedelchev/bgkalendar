c0660db9cbac4a59ca897d38326a11b261ed9614
Yordan Nedelchev <yordan.nedelchev@hotmail.com>
2019-01-06 01:37:45 +0200

    Translate Bogoyavlenie Yodanovden

--- phpsite/infogr/06-01.php
+++ phpsite/infogr/06-01.php
@@ -1,1 +1,15 @@
-Богоявление (<i>Йордановден</i>)
+<?php if ($lang == 'bg') : ?>
+<a href="https://bg.wikipedia.org/wiki/Богоявление">Богоявление</a> (<i>Йордановден</i>)
+
+<?php elseif ($lang == 'en') : ?>
+<a href="https://en.wikipedia.org/wiki/Epiphany_(holiday)">Epiphany</a> (<i>Yordanday</i>)
+
+
+<?php elseif ($lang == 'de') : ?>
+<a href="https://de.wikipedia.org/wiki/Erscheinung_des_Herrn">Erscheinung des Herrn</a> (<i>Jordanischtag</i>)
+
+<?php elseif ($lang == 'ru') : ?>
+<a href="https://ru.wikipedia.org/wiki/Богоявление">Богоявление</a> (<i>Йордановден</i>)
+
+
+<?php endif ?>
